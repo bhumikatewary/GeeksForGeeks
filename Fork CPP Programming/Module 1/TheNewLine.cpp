ff64e0734238998699fa538bff2448e2529f0ce6
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2022-02-03 23:14:49 +0530

    NewLine

---
+++ Fork CPP Programming/Module 1/TheNewLine.cpp
@@ -0,0 +1,29 @@
+//Initial Template for C++
+
+#include<iostream>
+using namespace std;
+
+void printNewLine();
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+// Function to print each word in new line
+void printNewLine(){
+    cout<<"Geeks"<<endl; 
+    cout<<"for"<<endl; 
+    cout<<"Geeks"<<endl;
+    // Your code here
+    
+}
+
+// { Driver Code Starts.
+
+int main(){
+    
+    //function call
+    printNewLine();
+    
+}
+  // } Driver Code Ends
